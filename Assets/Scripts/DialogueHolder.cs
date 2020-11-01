dbf47bd7dc224ac6130712147f8c8d9f20035441
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2020-11-01 20:04:13 -0300

    01-11-20 - Dialogue System

---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour
+{
+    public string characterName;
+    public Sprite characterSprite;
+    public string[] portugues;
+    public string[] kaingang;
+    public float triggerDistance = 1;
+
+    private Transform player;
+    private DialogueManager dialogueManager;
+    private GameObject interactionBtn;
+
+    private bool triggered = false; // controla se o dialogo ja foi acionado
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        dialogueManager = GameObject.Find("/Canvas/Dialogue System").GetComponent<DialogueManager>();
+        interactionBtn = GameObject.Find("/Canvas/Dialogue System/Interaction Btn").gameObject;
+    }
+
+    private void OnBecameVisible() // comeca a conferir a distancia para o player
+    {
+        InvokeRepeating("CheckDistance", 0.1f, 0.1f);
+    }
+
+    private void OnBecameInvisible() // para de conferir a distancia
+    {
+        CancelInvoke("CheckDistance");
+    }
+
+    private void CheckDistance()
+    {
+        float distance = Vector3.Distance(this.transform.position, player.position);
+        if (!triggered && distance < triggerDistance) // proximo do personagem e ainda nao enviou as sentencas
+        {
+            SendSentences();
+            interactionBtn.SetActive(true);
+            triggered = true;
+        } 
+        else if (triggered && distance > triggerDistance) // distante do personagem
+        {
+            triggered = false;
+            interactionBtn.SetActive(false);
+        }
+    }
+
+    private void SendSentences()
+    {
+        dialogueManager.sentences.Clear(); // limpa a lista
+        dialogueManager.characterSprite.sprite = characterSprite;
+        dialogueManager.characterName.text = characterName;
+        interactionBtn.SetActive(false); // esconde o botao de iniciar dialogo
+
+        if (PlayerPrefs.GetInt("Idioma") == 0) // portugues
+        {
+            for (int i = 0; i < portugues.Length; i++)
+            {
+                dialogueManager.sentences.Add(portugues[i]);
+            }
+        }
+        else if(PlayerPrefs.GetInt("Idioma") == 1) // kaingang
+        {
+            for (int i = 0; i < kaingang.Length; i++)
+            {
+                dialogueManager.sentences.Add(kaingang[i]);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public List<string> sentences;
+    public Image characterSprite;
+    public Text characterName;
+
+    public Text dialogueText;
+    public GameObject ContinueButton;
+    public GameObject backButton;
+
+    private short currentIndex;
+    private Animator anim;
+    private GameObject display;
+
+    void Start()
+    {
+        currentIndex = 0;
+        anim = this.transform.Find("Display/Dialogue Display").GetComponent<Animator>();
+        display = this.transform.Find("Display").gameObject;
+    }
+
+    private void Update()
+    {
+        /*if (!beingHandled && ContinueButton.activeSelf && (Input.GetKeyDown("return") || Input.GetKeyDown("enter")))
+        {
+            Debug.Log("Pressionou enter");
+            DisplayNextSentence();
+        }*/
+    }
+
+    public void Begin()
+    {
+        anim.SetTrigger("Open");
+        Continue();
+    }
+
+    public void Continue()
+    {
+        if (currentIndex < sentences.Count) { DisplayNextSentence(); }
+        else { StartCoroutine(Close()); }
+    }
+
+    IEnumerator Close()
+    {
+        anim.SetTrigger("Close");
+        yield return new WaitForSeconds(0.5f);
+        ResetSentences();
+        display.SetActive(false);
+    }
+
+    public void DisplayNextSentence()
+    {
+        backButton.SetActive(currentIndex > 0); // nao exibe o botao de voltar na primeira mensagem
+
+        StopAllCoroutines(); // Para de exibir todas as mensagens
+
+        StartCoroutine(TypeSentence(sentences[currentIndex])); // Comeca a exibir a mensagem atual letra por letra
+
+        currentIndex++; // Incrementa o numero da mensagem atual
+    }
+
+    public void DisplayPreviousSentence()
+    {
+        currentIndex -= 2; // Decrementa o index para a mensagem anterior
+        if (currentIndex > 0) // Caso essa nao seja a primeira mensagem, ativa o botao de voltar
+            backButton.SetActive(true);
+        else if (currentIndex == 0)
+            backButton.SetActive(false);
+
+        backButton.SetActive(currentIndex > 0); // nao exibe o botao de voltar na primeira mensagem
+
+        StopAllCoroutines(); // Para de exibir todas as mensagens
+
+        StartCoroutine(TypeSentence(sentences[currentIndex])); // Comeca a exibir a mensagem atual letra por letra
+
+        currentIndex++; // Incrementa o numero da mensagem atual
+
+    }
+
+    IEnumerator TypeSentence(string sentence) // Exibe a mensagem letra por letra
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.03f); // Intervalo de dempo entre cada letra
+        }
+    }
+
+    public void ResetSentences()
+    {
+        currentIndex = 0;
+    }
+
+}

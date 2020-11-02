4ad604b7955d0dcc9dd30100ae90ec3a31280559
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2020-11-02 20:36:11 -0300

    02-11-20 - TaskSlot and TaskMaker
    Elder 3D model with idle animation

---
+++ Assets/Scripts/TaskMaker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskMaker : MonoBehaviour
+{
+    public GameObject taskPrefab;
+    public Sprite icon;
+    public string description;
+    public int target;
+    private Transform parent;
+
+    private void Start()
+    {
+        parent = GameObject.Find("/Canvas/Task System/Background").transform;
+        if(parent == null) { Debug.LogError("TaskMaker nao encontrou o parent das tasks, conferir nome"); }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            GenerateTask();
+        }
+    }
+
+    public void GenerateTask()
+    {
+        GameObject task = Instantiate(taskPrefab, Vector3.zero, Quaternion.identity);
+        task.transform.parent = parent;
+        task.transform.localScale = Vector3.one;
+        TaskSlot taskScript = task.GetComponent<TaskSlot>();
+        taskScript.icon.sprite = icon;
+        taskScript.descriptionText.text = description;
+        taskScript.target = target;
+    }
+}
---
+++ Assets/Scripts/TaskSlot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskSlot : MonoBehaviour
+{
+    public Image icon; // icone da task
+    public Text descriptionText; // descricao da task
+    public int target; // valor objetivo
+
+    private int current = 0; // valor atual
+    private RectTransform progressBar; // a barra de progresso na UI
+    private int progressBarMaxSize; // tamanho maximo da barra em pixels
+
+    void Start()
+    {
+        progressBar = this.transform.Find("Progress Bar").GetComponent<RectTransform>();
+        progressBarMaxSize = (int)progressBar.sizeDelta.x;
+        RefreshProgressBar();
+    }
+
+    public void IncrementProgress(int value) // chamado para incrementar um valor x no progresso
+    {
+        current += value;
+        RefreshProgressBar();
+    }
+
+    public void RefreshProgressBar() // atualiza o tamanho da barra
+    {
+        progressBar.sizeDelta = new Vector3((current * progressBarMaxSize) / target, progressBar.sizeDelta.y);
+    }
+}
